ffff566f1a9f4c102a6f8ee091be258d5dd96b98
Matheus-Matta <matheuseduardo3004@gmail.com>
2026-08-03 12:44:40 -0300

    Atualiza webhook de produção do n8n

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 // Edite somente os valores abaixo antes de publicar.
 window.SITE_CONFIG = {
-  webhookUrl: "https://n8n.starchats.com.br/webhook-test/ffe2a53a-e2ee-4a2a-bd18-01275ed3b3c3",
+  webhookUrl: "https://n8nwebhook.starchats.com.br/webhook/ffe2a53a-e2ee-4a2a-bd18-01275ed3b3c3",
   mapsUrl: "https://www.google.com/maps/search/?api=1&query=Divert+Eventos"
 };
